f98d9e1694bd2bebf46f9fc066dd024f27518a69
Bibelsammler <bibelsammler@uni.de>
2015-08-29 23:17:58 +0200

    first commit

---
+++ metadata.js
@@ -0,0 +1,674 @@
+biblename = "Das Neue Testament in deutscher Fassung und die Psalmen: Lexikon [Herbert Jantzen] (2011) [www.sermon-online.de]";
+metadata = [{
+abbr:"L1",
+short:"Vorwort",
+long:"Vorwort",
+osis:"x-Intr",
+type:"meta",
+nt:false,
+chapters:1
+},{
+abbr:"L2",
+short:"Ankunft",
+long:"Ankunft",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L3",
+short:"antwortete",
+long:"antwortete",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L4",
+short:"Bereich des Todes",
+long:"Bereich des Todes",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L5",
+short:"Bote",
+long:"Bote",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L6",
+short:"Buße",
+long:"Buße",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L7",
+short:"Christus",
+long:"Christus",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L8",
+short:"Dank; Dankbarkeit; dankbar",
+long:"Dank; Dankbarkeit; dankbar",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L9",
+short:"Ewigkeit; in Ewigkeit; in ‹alle› Ewigkeit",
+long:"Ewigkeit; in Ewigkeit; in ‹alle› Ewigkeit",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L10",
+short:"ewig",
+long:"ewig",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L11",
+short:"Feuerhölle",
+long:"Feuerhölle",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L12",
+short:"Freue dich!; Freut euch!",
+long:"Freue dich!; Freut euch!",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L13",
+short:"Gesalbter",
+long:"Gesalbter",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L14",
+short:"Hoher Rat",
+long:"Hoher Rat",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L15",
+short:"Hölle",
+long:"Hölle",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L16",
+short:"Hosanna",
+long:"Hosanna",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L17",
+short:"Hurerei",
+long:"Hurerei",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L18",
+short:"Mann",
+long:"Mann",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L19",
+short:"Offenbarung; offenbaren; geoffenbart",
+long:"Offenbarung; offenbaren; geoffenbart",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L20",
+short:"Prätorium",
+long:"Prätorium",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L21",
+short:"soll; du sollst; ..",
+long:"soll; du sollst; ..",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L22",
+short:"Todesbereich",
+long:"Todesbereich",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L23",
+short:"Unzucht",
+long:"Unzucht",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L24",
+short:"Vater werden",
+long:"Vater werden",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L25",
+short:"Vollmacht",
+long:"Vollmacht",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L26",
+short:"Weltzeit",
+long:"Weltzeit",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L27",
+short:"Wort",
+long:"Wort",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"L28",
+short:"Zahlreiche Mengen",
+long:"Zahlreiche Mengen",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP2",
+short:"aufrichtig; Aufrichtige; Aufrichtigkeit [Psalmen] [Psalmen]",
+long:"aufrichtig; Aufrichtige; Aufrichtigkeit [Psalmen] [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP3",
+short:"Ausspruch [Psalmen] [Psalmen]",
+long:"Ausspruch [Psalmen] [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP4",
+short:"Bereich des Todes [Psalmen]",
+long:"Bereich des Todes [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP5",
+short:"Bergungsort [Psalmen]",
+long:"Bergungsort [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP6",
+short:"Bote [Psalmen]",
+long:"Bote [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP7",
+short:"Ehre [Psalmen]",
+long:"Ehre [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP8",
+short:"Ehrfurchtslose(r); ehrfurchtslos [Psalmen]",
+long:"Ehrfurchtslose(r); ehrfurchtslos [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP9",
+short:"elend; Elende(r); Elend [Psalmen]",
+long:"elend; Elende(r); Elend [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP10",
+short:"Flehen [Psalmen]",
+long:"Flehen [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP11",
+short:"Friede [Psalmen]",
+long:"Friede [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP12",
+short:"Fromme(r); fromm [Psalmen]",
+long:"Fromme(r); fromm [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP13",
+short:"gebeugt; Gebeugte; Gebeugtsein [Psalmen]",
+long:"gebeugt; Gebeugte; Gebeugtsein [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP14",
+short:"gerade; Gerade; Geradheit [Psalmen]",
+long:"gerade; Gerade; Geradheit [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP15",
+short:"Gnade [Psalmen]",
+long:"Gnade [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP16",
+short:"gnädig; sei mir gnädig [Psalmen]",
+long:"gnädig; sei mir gnädig [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP17",
+short:"Gott [Psalmen]",
+long:"Gott [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP18",
+short:"Hallelu-Jah [Psalmen]",
+long:"Hallelu-Jah [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP19",
+short:"halten [Psalmen]",
+long:"halten [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP20",
+short:"Hand; Hände [Psalmen]",
+long:"Hand; Hände [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP21",
+short:"Heiden [Psalmen]",
+long:"Heiden [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP22",
+short:"Heil [Psalmen]",
+long:"Heil [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP23",
+short:"Heilige(r) [Psalmen]",
+long:"Heilige(r) [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP24",
+short:"herabschauen; schaue herab [Psalmen]",
+long:"herabschauen; schaue herab [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP25",
+short:"<d>Herr</>; Herr [Psalmen]",
+long:"<d>Herr</>; Herr [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP26",
+short:"Herrlichkeit [Psalmen]",
+long:"Herrlichkeit [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP27",
+short:"Hilfe, Helfen [Psalmen]",
+long:"Hilfe, Helfen [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP28",
+short:"Lauterkeit ; lauter; Lautere [Psalmen]",
+long:"Lauterkeit ; lauter; Lautere [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP29",
+short:"Leitender Musiker [Psalmen]",
+long:"Leitender Musiker [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP30",
+short:"loben [Psalmen]",
+long:"loben [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP31",
+short:"Mächtiger [Psalmen]",
+long:"Mächtiger [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP32",
+short:"Mensch [Psalmen]",
+long:"Mensch [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP33",
+short:"Michtam [Psalmen]",
+long:"Michtam [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP34",
+short:"Musiker [Psalmen]",
+long:"Musiker [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP35",
+short:"Reden [Psalmen]",
+long:"Reden [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP36",
+short:"Retter [Psalmen]",
+long:"Retter [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP37",
+short:"rechtschaffen [Psalmen]",
+long:"rechtschaffen [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP38",
+short:"richtig [Psalmen]",
+long:"richtig [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP39",
+short:"schaue herab [Psalmen]",
+long:"schaue herab [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP40",
+short:"Sela [Psalmen]",
+long:"Sela [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP41",
+short:"selig [Psalmen]",
+long:"selig [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP42",
+short:"singen; singen ‹und spielen› [Psalmen]",
+long:"singen; singen ‹und spielen› [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP43",
+short:"spielen; singen ‹und spielen› [Psalmen]",
+long:"spielen; singen ‹und spielen› [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP44",
+short:"Stufenlied [Psalmen]",
+long:"Stufenlied [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP45",
+short:"Treue [Psalmen]",
+long:"Treue [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP46",
+short:"Übeltäter [Psalmen]",
+long:"Übeltäter [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP47",
+short:"untadelig [Psalmen]",
+long:"untadelig [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP48",
+short:"Unterweisung [Psalmen]",
+long:"Unterweisung [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP49",
+short:"Verordnungen [Psalmen]",
+long:"Verordnungen [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP50",
+short:"vollkommen [Psalmen]",
+long:"vollkommen [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP51",
+short:"Völker [Psalmen]",
+long:"Völker [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP52",
+short:"Wahrheit [Psalmen]",
+long:"Wahrheit [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP53",
+short:"Weisung [Psalmen]",
+long:"Weisung [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP54",
+short:"Welt [Psalmen]",
+long:"Welt [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP55",
+short:"Wohlergehen [Psalmen]",
+long:"Wohlergehen [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP56",
+short:"Wort [Psalmen]",
+long:"Wort [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+},{
+abbr:"LP57",
+short:"Wunder, Wundertaten [Psalmen]",
+long:"Wunder, Wundertaten [Psalmen]",
+osis:"x-Dict",
+type:"dict",
+nt:true,
+chapters:1
+}];
